d19868defa8c9222c17ca5b3e8d5a5483f935985
박금주 <gj@MacBook-Pro-4.local>
2026-04-05 16:48:52 +0900

    GAS URL 업데이트

--- 상세페이지/apps-script/Code.js
+++ 상세페이지/apps-script/Code.js
@@ -14,7 +14,7 @@ const SPREADSHEET_MAP = {
   'chilgok': '12RJAZ8CdwR5yJjmbetusxvTxuLxweABbP-CiTzEbctM',       // 국립칠곡숲체원
 };
 const SHEET_NAME = '예약문의';
-const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
+const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 
 /**
  * POST 요청 처리 (폼 데이터 수신)
